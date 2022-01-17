410e02378c9a64ff47c3fdb4d5e26c275854ab17
unknown <william20041206@hotmail.com>
2022-01-17 10:42:06 +0800

    Move the location of the policy file

--- src/main/java/com/pvpin/pvpincore/modules/boot/BootStrap.java
+++ src/main/java/com/pvpin/pvpincore/modules/boot/BootStrap.java
@@ -30,7 +30,11 @@ import io.github.classgraph.ClassGraph;
 import io.github.classgraph.ScanResult;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.security.Policy;
+import java.util.Set;
 
 /**
  * @author William_Shi
@@ -45,8 +49,37 @@ public class BootStrap {
         // Download libraries.
         PVPINLoggerFactory.getCoreLogger().info("依赖加载完毕");
 
-        PVPINCore.getCoreInstance().saveResource("pvpin.policy", true);
-        System.setProperty("java.security.policy", new File(PVPINCore.getCoreInstance().getDataFolder(), "pvpin.policy").toURI().toURL().toString());
+        PVPINCore.getCoreInstance().saveResource("com/pvpin/pvpincore/modules/js/security/pvpin.policy", true);
+        File policyFileOrigin = new File(PVPINCore.getCoreInstance().getDataFolder(), "com/pvpin/pvpincore/modules/js/security/pvpin.policy");
+        File policyFileNew = new File(PVPINCore.getCoreInstance().getDataFolder(), "security/pvpin.policy");
+        // Save the policy file to a local folder, and move it to an appropriate location.
+        // Delete the com/ directory after use.
+        boolean bool = policyFileNew.exists() ? policyFileNew.delete() : policyFileNew.getParentFile().mkdirs();
+        policyFileOrigin.renameTo(policyFileNew);
+        Files.walkFileTree(new File(PVPINCore.getCoreInstance().getDataFolder(), "com").toPath(), Set.of(FileVisitOption.FOLLOW_LINKS), Integer.MAX_VALUE, new FileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                Files.deleteIfExists(file);
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+                return FileVisitResult.CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+                Files.deleteIfExists(dir);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+        System.setProperty("java.security.policy", policyFileNew.toURI().toURL().toString());
         Policy.getPolicy().refresh();
         System.setSecurityManager(new JSSecurityManager());
         // Replace the old security manager.
